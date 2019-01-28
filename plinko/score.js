7db398bab2432de9e250f3d488e1d1d391abbae7
Justin Cook <justin.cook.dev@gmail.com>
2019-01-28 10:48:16 -0800

    KNN sol'n from other directory

--- plinko/score.js
+++ plinko/score.js
@@ -1,8 +1,75 @@
+// [ dropPosition, bounciness, size, bucketLabel ]
+const outputs = [];
+
 function onScoreUpdate(dropPosition, bounciness, size, bucketLabel) {
-  // Ran every time a balls drops into a bucket
+  outputs.push([dropPosition, bounciness, size, bucketLabel]);
 }
 
 function runAnalysis() {
-  // Write code here to analyze stuff
+  const testSetSize = 100;
+  const k = 10;
+
+  _.range(0, 3).forEach(feature => {
+    const data = _.map(outputs, row => [row[feature], _.last(row)]);
+    const [testData, trainingData] = shuffleDataset(minMax(data, 1), testSetSize);
+    const accuracy = _.chain(testData)
+      .filter(testPoint => knn(trainingData, _.initial(testPoint), k) === _.last(testPoint))
+      .size()
+      .divide(testSetSize)
+      .value();
+
+    console.log(`For feature of ${feature} Accuracy is, ${accuracy}`);
+  });
 }
 
+function knn(data, point, k) {
+  return _.chain(data)
+    .map(row => {
+      return [
+        distance(_.initial(row), point),
+        _.last(row)
+      ]
+    })
+    .sortBy(row => row[0])
+    .slice(0, k)
+    .countBy(row => row[1])
+    .toPairs()
+    .sortBy(row => row[1])
+    .last()
+    .first()
+    .parseInt()
+    .value();
+}
+
+function distance(pointA, pointB) {
+  return _.chain(pointA)
+    .zip(pointB)
+    .map(([a, b]) => (a - b) ** 2)
+    .sum()
+    .value() ** 0.5;
+}
+
+function shuffleDataset(data, testCount) {
+  const shuffled = _.shuffle(data);
+
+  const testData = _.slice(shuffled, 0, testCount);
+  const trainingData = _.slice(shuffled, testCount);
+
+  return [testData, trainingData];
+}
+
+function minMax(data, featureCount) {
+  const cloneData = _.cloneDeep(data);
+
+  for (let i = 0; i < featureCount; i++) {
+    const column = _.map(cloneData, row => row[i]);
+    const minimum = _.min(column);
+    const maximum = _.max(column);
+
+    for (let j = 0; j < cloneData.length; j++) {
+      cloneData[j][i] = (cloneData[j][i] - minimum) / (maximum - minimum)
+    }
+  }
+
+  return cloneData;
+}
